13b91c66eefee1f88ba27f7c26c588d2cafc0d25
David <david.mtz00@hotmail.com>
2021-02-02 20:08:07 -0600

    :bulb: Add routing functionality example comment

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -2,17 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 
+/* If we wanted to set routing functionality, we could tell Angular which component to load when navigating like so:
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{
+  // Tell to load HomeComponent, when navigating in the browser to domain.com/home
+  // We keep adding routes for each components we want to navigate
+  path: [
+    'home',
+    component: HomeComponent,
+  ]
+}]
+
+@NgModule ({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
+  ],
+})
+
+export class AppRoutingModule { }
+* */
+
+/* @NgModule is Angular. The rest is a standard JS/TS class
+* It helps organize an application into cohesive blocks of functionality */
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ],
 })
 export class AppModule { }
