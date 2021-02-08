aaec2cd9af8fbed30bb8c019c525383d3fdd12cb
David <Gothoemic123!>
2021-02-08 15:01:03 -0600

    :construction: Implement GET request
    Signed-off-by: David <Gothoemic123!>

--- local-weather-app/src/app/weather/weather.service.ts
+++ local-weather-app/src/app/weather/weather.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
+
 /* This file contains the `@Injectable` decorator above the class definition, which makes it possible to inject this service into other components, leveraging Angular's provider system. This ensures that our service is a singleton, meaning it is instantiated once, no matter how many times it is injected elsewhere */
 import { Injectable } from '@angular/core'
+import { environment } from '../../environments/environment'
 
 interface ICurrentWeatherData {
   weather: [
@@ -25,4 +27,25 @@ interface ICurrentWeatherData {
 })
 export class WeatherService {
   constructor(private httpClient: HttpClient) {}
+
+  getCurrentWeather(city: string, country: string) {
+    /* We could of written our GET request like this:
+
+      return this.httpClient
+       .get<ICurrentWeatherData>(
+          `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?` +
+          `q=${city},${country}&appid=${environment.appId}`
+       )
+
+       However, using a long and complicated string is an error-prone process. Instead, we can use the `HttpParams` object to build the URL programmatically like this:
+    */
+    const uriParams = new HttpParams()
+      .set('q', `${city},${country}`)
+      .set('appid', environment.appId)
+
+    return this.httpClient.get<ICurrentWeatherData>(
+      `${environment.baseUrl}api.openweathermap.org/data/2.5/weather`,
+      { params: uriParams }
+    )
+  }
 }
