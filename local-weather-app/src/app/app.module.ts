39d8bfa4eba62e067a7b54322de6ed5d982ce040
David Martinez <jdm150130@utdallas.edu>
2021-02-08 14:02:45 -0600

    :construction: Create interface to accept shape of API we'll call
    Signed-off-by: David <Gothoemic123!>

--- local-weather-app/src/app/app.module.ts
+++ local-weather-app/src/app/app.module.ts
@@ -3,11 +3,12 @@
 import { AppComponent } from './app.component'
 import { BrowserModule } from '@angular/platform-browser'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- local-weather-app/src/app/weather/weather.service.ts
+++ local-weather-app/src/app/weather/weather.service.ts
@@ -1,10 +1,28 @@
+import { HttpClient } from '@angular/common/http'
 /* This file contains the `@Injectable` decorator above the class definition, which makes it possible to inject this service into other components, leveraging Angular's provider system. This ensures that our service is a singleton, meaning it is instantiated once, no matter how many times it is injected elsewhere */
 import { Injectable } from '@angular/core'
+
+interface ICurrentWeatherData {
+  weather: [
+    {
+      description: string
+      icon: string
+    }
+  ]
+  main: {
+    temp: number
+  }
+  sys: {
+    country: string
+  }
+  dt: number
+  name: string
+}
 
 /* The `providedIn` property ensures that the root module provides the weather service in `app.module.ts` */
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 }
