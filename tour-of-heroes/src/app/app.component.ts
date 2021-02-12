934589d757fcdeb1eede741e1ba0df7ab5353e8d
David Martinez <jdm150130@utdallas.edu>
2021-02-12 13:34:52 -0600

    :tada: Begin the new tour of heroes project
    Signed-off-by: David <Gothoemic123!>

---
+++ tour-of-heroes/src/app/app.component.ts
@@ -0,0 +1,11 @@
+/* This is the component class file */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tour-of-heroes';
+}
