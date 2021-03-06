08481d984e0c324e5d631914ddc77be4f6a9f797
David Martinez <jdm150130@utdallas.edu>
2021-03-06 17:51:32 -0600

    :construction: Add product pagination

--- sports-store/src/app/store/store.component.ts
+++ sports-store/src/app/store/store.component.ts
@@ -10,13 +10,16 @@ import {ProductRepository} from "../model/product.repository"
 })
 export class StoreComponent {
   public selectedCategory = null
+  public productsPerPage = 4
+  public selectedPage = 1
 
   constructor(private repository: ProductRepository) {
   }
 
   /* The `products` and `categories` properties will be used to generate HTML content in the template, using data obtained from the repository */
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory)
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage)
   }
 
   get categories(): string[] {
@@ -25,5 +28,20 @@ export class StoreComponent {
 
   changeCategory(newCategory?: string) {
     this.selectedCategory = newCategory
+  }
+
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage))
+      .fill(0).map((x, i) => i + 1)
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize)
+    this.changePage(1)
   }
 }
