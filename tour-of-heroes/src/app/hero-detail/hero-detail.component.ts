764e1a4dd7a842618e27b9c49e56db6e8f77107d
David Martinez <jdm150130@utdallas.edu>
2021-03-08 16:23:52 -0600

    :construction: Add the @Input() hero property

--- tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core'
+import {Hero} from '../hero'
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +7,11 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  /* `hero` property must be an *Input* property, annotated with the `@Input()` decorator, because the *external* `HeroesComponent` will use property binding to bind the `hero` property in this component to `selectedHero` in `HeroesComponent` like this:
+  * `<app-hero-detail [hero]="selectedHero"></app-hero-detail>`
+  *
+  * This is a common pattern when sharing data between one or more child components */
+  @Input() hero?: Hero
 
   constructor() {
   }
