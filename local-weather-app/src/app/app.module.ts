6d1faf1fd5b2e68a20dc80195e721d68f614d38f
David Martinez <jdm150130@utdallas.edu>
2021-03-24 16:54:11 -0500

    :construction: Install Angular Material

--- local-weather-app/src/app/app.module.ts
+++ local-weather-app/src/app/app.module.ts
@@ -1,15 +1,17 @@
 /* When Angular begins execution, after `main.ts` loads this file, this file then loads all its dependencies,
  * and renders within the `<app-root>` element */
-import { AppComponent } from './app.component'
-import { BrowserModule } from '@angular/platform-browser'
-import { CurrentWeatherComponent } from './current-weather/current-weather.component'
-import { HttpClientModule } from '@angular/common/http'
-import { NgModule } from '@angular/core'
+import {AppComponent} from './app.component'
+import {BrowserModule} from '@angular/platform-browser'
+import {CurrentWeatherComponent} from './current-weather/current-weather.component'
+import {HttpClientModule} from '@angular/common/http'
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
