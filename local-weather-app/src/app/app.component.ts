6635e177774aebfbd531ead7a49854082a1b5a4e
David Martinez <jdm150130@utdallas.edu>
2021-03-28 00:24:16 -0500

    :construction: Add side navigation layout

--- local-weather-app/src/app/app.component.ts
+++ local-weather-app/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core'
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+    <div style="text-align: center">
+      <mat-toolbar>
+        <span>Weather 24/7</span>
+      </mat-toolbar>
+      <div><b>Some type of slogan, or tagline! I'm going to be changing this tho 🥳</b></div>
+      <app-current-weather></app-current-weather>
+      <div>Weekly Forecast</div>
+    </div>`,
 })
 export class AppComponent {
   title = 'local-weather-app'
--- local-weather-app/src/app/app.module.ts
+++ local-weather-app/src/app/app.module.ts
@@ -7,11 +7,18 @@ import {HttpClientModule} from '@angular/common/http'
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module'
-import {FlexLayoutModule} from '@angular/flex-layout'
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {SideNavComponent} from './side-nav/side-nav.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list'
 
 @NgModule({
-  declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MaterialModule, FlexLayoutModule],
+  declarations: [AppComponent, CurrentWeatherComponent, SideNavComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MaterialModule, FlexLayoutModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
