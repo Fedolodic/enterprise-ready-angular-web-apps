e6de964e3ef8bd95c78e0282ca4fc1143172fe16
David Martinez <jdm150130@utdallas.edu>
2021-03-28 17:52:16 -0500

    :construction: Improve Material header and content

--- local-weather-app/src/app/app.component.ts
+++ local-weather-app/src/app/app.component.ts
@@ -10,16 +10,14 @@ import {Component} from '@angular/core'
       <div><b>24-Hour Forecasts</b></div>
       <div fxLayout="row">
         <div fxFlex></div>
-        <div fxFlex="300px">
-          <mat-card>
-            <mat-card-title>
-              Current Weather
-            </mat-card-title>
-            <mat-card-content>
-              <app-current-weather></app-current-weather>
-            </mat-card-content>
-          </mat-card>
-        </div>
+        <mat-card fxFlex="300px">
+          <mat-card-header>
+            <mat-card-title>Current Weather</mat-card-title>
+          </mat-card-header>
+          <mat-card-content>
+            <app-current-weather></app-current-weather>
+          </mat-card-content>
+        </mat-card>
         <div fxFlex></div>
       </div>
       <div>Weekly Forecast</div>
