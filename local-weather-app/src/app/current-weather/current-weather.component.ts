4df5ccc277ffc5dfd367daec3bf6ce810bff2368
David Martinez <jdm150130@utdallas.edu>
2021-03-29 15:06:14 -0500

    :pencil2: Fix typo

--- local-weather-app/src/app/current-weather/current-weather.component.ts
+++ local-weather-app/src/app/current-weather/current-weather.component.ts
@@ -1,8 +1,8 @@
 /* This file contains the `@Component` decorator above the class definition, and is the glue that ties together the CSS, HTML, and JavaScript code. The class itself can be considered the ViewModel, pulling data from services and performing any necessary transformations to expose sensible bindings for the View */
-import { Component, OnInit } from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 
-import { ICurrentWeather } from '../interfaces'
-import { WeatherService } from '../weather/weather.service'
+import {ICurrentWeather} from '../interfaces'
+import {WeatherService} from '../weather/weather.service'
 
 @Component({
   selector: 'app-current-weather',
@@ -11,7 +11,7 @@ import { WeatherService } from '../weather/weather.service'
 })
 export class CurrentWeatherComponent implements OnInit {
   /* We add the `!` syntax for the common-ish cases where you can't guarantee that the value will be defined immediately
-   * It is an escape hatch, adn shouldn't be relied on, as it can make your code less safe
+   * It is an escape hatch, and shouldn't be relied on, as it can make our code less safe
    * A default value is usually preferred
    * It's good to know that it exists though */
   current!: ICurrentWeather
