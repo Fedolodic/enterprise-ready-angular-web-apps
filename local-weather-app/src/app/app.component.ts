07dec7ca8e09e176b7dc9931fb621fa9236f64ba
David Martinez <jdm150130@utdallas.edu>
2021-03-29 13:38:42 -0500

    :construction: Apply typography

--- local-weather-app/src/app/app.component.ts
+++ local-weather-app/src/app/app.component.ts
@@ -12,7 +12,9 @@ import {Component} from '@angular/core'
         <div fxFlex></div>
         <mat-card fxFlex="300px">
           <mat-card-header>
-            <mat-card-title>Current Weather</mat-card-title>
+            <mat-card-title>
+              <div class="mat-headline">Current Weather</div>
+            </mat-card-title>
           </mat-card-header>
           <mat-card-content>
             <app-current-weather></app-current-weather>
